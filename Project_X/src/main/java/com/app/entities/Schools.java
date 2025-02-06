d2180d66a273556b3b7ba09ff96ce97ace21f66a
Pratik <143311891+PratikDakhole10@users.noreply.github.com>
2025-02-06 17:22:32 +0530

    Exceptions and Repositories

--- Project_X/src/main/java/com/app/entities/Schools.java
+++ Project_X/src/main/java/com/app/entities/Schools.java
@@ -1,5 +1,57 @@
 package com.app.entities;
 
-public class Schools {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Schools {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int schoolId;
+	private Students studentId;
+	private String SchoolName;
+	private Drivers driverId;
+	private Parents parentId;
+	
+	
+	public Schools(int schoolId, Students studentId, String schoolName, Drivers driverId, Parents parentId) {
+		super();
+		this.schoolId = schoolId;
+		this.studentId = studentId;
+		SchoolName = schoolName;
+		this.driverId = driverId;
+		this.parentId = parentId;
+	}
+	public int getSchoolId() {
+		return schoolId;
+	}
+	public void setSchoolId(int schoolId) {
+		this.schoolId = schoolId;
+	}
+	public Students getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(Students studentId) {
+		this.studentId = studentId;
+	}
+	public String getSchoolName() {
+		return SchoolName;
+	}
+	public void setSchoolName(String schoolName) {
+		SchoolName = schoolName;
+	}
+	public Drivers getDriverId() {
+		return driverId;
+	}
+	public void setDriverId(Drivers driverId) {
+		this.driverId = driverId;
+	}
+	public Parents getParentId() {
+		return parentId;
+	}
+	public void setParentId(Parents parentId) {
+		this.parentId = parentId;
+	}
 }
---
+++ Project_X/src/main/java/com/app/repo/SchoolsRepository.java
@@ -0,0 +1,9 @@
+package com.app.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.entities.Schools;
+
+public interface SchoolsRepository extends JpaRepository<Schools,Long>{
+
+}
