0053c951817fdb6092f1c88180b55f17eb17893d
Mahendra Sunil Waghchaure <162361329+MahendraSWaghchaure@users.noreply.github.com>
2025-02-10 23:51:32 +0530

    Update StudentsService.java

--- Project_X/src/main/java/com/app/service/StudentsService.java
+++ Project_X/src/main/java/com/app/service/StudentsService.java
@@ -1,17 +1,24 @@
 package com.app.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.app.entities.Students;
 import com.app.repo.StudentsRepository;
 
 @Service
 public class StudentsService {
 	@Autowired
-	private StudentsRepository studentsrepo;
+	private static  StudentsRepository studentsrepo;
 	
 //	public StudentsService saveProfile(StudentsService profile) {
 //		return studentsrepo .save(profile);
 //	}
 	
+	public static List<Students> getAllStudents() {
+		// TODO Auto-generated method stub
+		return studentsrepo.findAll();
+	}
 }
